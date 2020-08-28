44f28e198541b5817ca2a04c657e698703c21c4d
Edward Bustos <esbustosg@eservicios.indracompany.com>
2020-08-28 12:29:28 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Typography variant="h1">E-Commerce</Typography>
+      <Typography variant="h5">beta</Typography>
+    </div>
+  );
+}
+
+export default App;
