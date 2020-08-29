463563a56abba9e0781c3cdb33a891ef05f3cc42
jonnathan.baquero <jonnathan.baquero301@outlook.es>
2020-08-28 21:32:05 -0500

    Updates

---
+++ precache-manifest.b1a686357b11aec8a1ba67afb78f7522.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b178d8902e63f21302e4749db7f62e6",
+    "url": "/ecommerce-beta/index.html"
+  },
+  {
+    "revision": "2199fd14111d803acb55",
+    "url": "/ecommerce-beta/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "6a8009d964252af8f9df",
+    "url": "/ecommerce-beta/static/js/2.74bea23c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce-beta/static/js/2.74bea23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2199fd14111d803acb55",
+    "url": "/ecommerce-beta/static/js/main.604848f2.chunk.js"
+  },
+  {
+    "revision": "cb013bfece970ce421e2",
+    "url": "/ecommerce-beta/static/js/runtime-main.bd40def3.js"
+  }
+]);
